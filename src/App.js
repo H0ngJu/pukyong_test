84b15124ce8dd0aa2392a60dc4c15188e217a317
이홍주 <hongju4385@gmail.com>
2023-02-05 14:01:14 +0900

    모바일/pc가 다름

--- src/App.js
+++ src/App.js
@@ -1,18 +1,49 @@
-import './style.css';
-import { useState } from 'react';
+import './Home.css';
+import React,{useEffect} from 'react';
+//import { useState } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Typepage from './type';
+//import Typepage from './type';
+import Homepage from './Home';
+//import { BrowserView, MobileView } from 'react-device-detect';
+
 
 export default function App() {
+  function setScreenSize() {
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty("--vh", `${vh}px`);
+  }
+  useEffect(() => {
+    setScreenSize();
+  });
 
+  // return(
+  //   <div className='App'>
+  //     <BrowserView>
+  //     데스크톱브라우저!
+  //     </BrowserView>
+  //     <MobileView>
+  //       모바일브라우저!
+  //     </MobileView>
+  //   </div>
+  // )
     return (
-      <div className="App">
+      <div className="Home">
         <BrowserRouter>
-          <Typepage />
+          <Homepage />
           <Routes>
-            <Route path={"/type"} element={<Typepage />}></Route>
+            <Route path={"/home"} element={<Homepage/>}></Route>
           </Routes>
         </BrowserRouter>
       </div>
     );
+    // return (
+    //   <div className="type">
+    //     <BrowserRouter>
+    //       <Typepage />
+    //       <Routes>
+    //         <Route path={"/type"} element={<Typepage/>}></Route>
+    //       </Routes>
+    //     </BrowserRouter>
+    //   </div>
+    // );
 }
--- src/Home.js
+++ src/Home.js
@@ -1,21 +1,120 @@
 import './Home.css';
+//import { useNavigate } from "react-router-dom";
+import { BrowserView, MobileView } from 'react-device-detect';
 
-function Home() {
+// function Title(props) {
+//   return (
+//       <div>
+//           <h2 className="Title">Pt.{props.id} {props.part}</h2>
+//       </div>
+//   )
+// }
+
+// function PartImg(props) {
+//   return (
+//       <div>
+//           <img src={props.url} className="PartImg"></img>
+//       </div>
+//   )
+// }
+
+// function PartNum(props) {
+//   return (
+//       <div className="PartNum">
+//           {props.id} / 7
+//       </div>
+//   )
+// }
+
+// function NextBtn(props) {
+//   return (
+//       <div>
+//           <button className="NextBtn" onClick={() => { }}>다음으로</button>
+//       </div>
+//   )
+// }
+
+// function Home() {
+//   return (
+//     <div class = "App">
+//       <head>
+//         <title>부경대 메인 페이지</title>
+//       </head>
+//         <body>
+//         <h1 class = "title">부경대 레벨 테스트</h1>
+//         <h4 class = "sub0">당신은 부경 새내기 인가요? 화석인가요?</h4>
+//         <h4 class = "sub1">지금 테스트 해보세요!</h4>
+//         <div class = "mainimg"><img src ='main.png' alt = "img" class = "mainimg"/></div>
+//         <div class = "button"><button type="button" onClick="location.href='http://www.naver.com'"class = "startButton">시작하기</button></div>
+//         <h6 class = "name">made by 컴공 21, 22</h6>
+//         </body>
+//     </div>
+//   );
+// }
+
+// function Home() {
+//   return (
+//     <div class="Home">
+//       <BrowserView>
+//         <div class="title">부경대 레벨 테스트</div>
+//         <div class="sub0">당신은 부경 새내기 인가요? 화석인가요?</div>
+//         <div class="sub1">지금 테스트 해보세요!</div>
+//         <div class="mainimg"><img src='main.png' alt='img' class='mainimg'></img></div>
+//         <div class='button'><button type='button' onClick="location.href = 'http://www.naver.com'" class="startButton">시작하기</button></div>
+//         <div class='name'>made by 컴공 21,</div>
+//       </BrowserView>
+//       <MobileView>
+//         <div class="Title">부경대 레벨 테스트</div>
+//         <div class="sub0">당신은 부경 새내기 인가요? 화석인가요?</div>
+//         <div class="sub1">지금 테스트 해보세요!</div>
+//         <div class="mainimg"><img src='main.png' alt='img' class='mainimg'></img></div>
+//         <div class='button'><button type='button' onClick={gotype} class="startButton">시작하기</button></div>
+//         <div class='name'>made by 컴공 21,</div>
+//       </MobileView>
+//     </div>
+
+//   )
+// }
+
+export default function Home(){
+  // const movePage = useNavigate;
+
+  // function gotype(){
+  //   movePage('/type');
+  // }
+
   return (
-    <div>
-      <head>
-        <title>부경대 메인 페이지</title>
-      </head>
-        <body>
-        <h1 class = "title">부경대 레벨 테스트</h1>
-        <h4 class = "sub0">당신은 부경 새내기 인가요? 화석인가요?</h4>
-        <h4 class = "sub1">지금 테스트 해보세요!</h4>
-        <div class = "mainimg"><img src ='main.png' alt = "img" class = "mainimg"/></div>
-        <div class = "button"><button type="button" onClick="location.href='http://www.naver.com'"class = "startButton">시작하기</button></div>
-        <h6 class = "name">made by 컴공 21, 22</h6>
-        </body>
+    <div class="Home">
+      <BrowserView>
+        <div class="title">부경대 레벨 테스트</div>
+        <div class="sub0">당신은 부경 새내기 인가요? 화석인가요?<br/>지금 테스트 해보세요!</div>
+        <div class="mainimg"><img src='main.png' alt='img' class='mainimg'></img></div>
+        <div class='button'><button type='button' onClick="location.href = 'http://www.naver.com'" class="startButton">시작하기</button></div>
+        <div class='name'>made by 컴공 21,</div>
+      </BrowserView>
+      <MobileView>
+        <div class="Title">부경대 레벨 테스트</div>
+        <div class="sub0">당신은 부경 새내기 인가요? 화석인가요?<br/>지금 테스트 해보세요!</div>
+        <div class="mainimg"><img src='main.png' alt='img' class='mainimg'></img></div>
+        <div class='button'><button type='button' onClick="location.href = 'http://www.naver.com'" class="startButton">시작하기</button></div>
+        <div class='name'>made by 컴공 21,</div>
+      </MobileView>
     </div>
   );
 }
 
-export default Home;
+//   export default function Home() {
+//     // const navigate = useNavigate();
+//     // function nextPage() {
+//     //     navigate('/user/home');
+//     // }
+
+//     return (
+//         <div className="App">
+//             <Title id="1" part="수강신청" />
+//             <PartImg url="https://www.pknu.ac.kr/upload/raonkeditordata/2022/08/18/20220818_102119098_72062.png" />
+//             <PartNum id="1" />
+//             <NextBtn onChangeMode={() => { }} />
+//         </div>
+//     );
+// }
