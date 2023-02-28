e9d760e8706ce1b97bc903b288fae323a6aafb2e
이홍주 <hongju4385@gmail.com>
2023-02-28 13:25:19 +0900

    2/28 commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import './Home.css';
-import React,{useEffect} from 'react';
+import "./Home.css";
+import "./quiz.css";
+import "./result.css";
+import React, { useEffect } from "react";
 //import { useState } from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-//import Typepage from './type';
-import Homepage from './Home';
-//import { BrowserView, MobileView } from 'react-device-detect';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+//import Quizpage from './quiz';
+import Resultpage from "./result_2";
+//import Homepage from './Home';
 
-
-export default function App() {
+export default function Result() {
   function setScreenSize() {
     let vh = window.innerHeight * 0.01;
     document.documentElement.style.setProperty("--vh", `${vh}px`);
@@ -15,24 +16,44 @@ export default function App() {
   useEffect(() => {
     setScreenSize();
   });
-    return (
-      <div className="Home">
-        <BrowserRouter>
-          <Homepage />
-          <Routes>
-            <Route path={"/home"} element={<Homepage/>}></Route>
-          </Routes>
-        </BrowserRouter>
-      </div>
-    );
-    // return (
-    //   <div className="type">
-    //     <BrowserRouter>
-    //       <Typepage />
-    //       <Routes>
-    //         <Route path={"/type"} element={<Typepage/>}></Route>
-    //       </Routes>
-    //     </BrowserRouter>
-    //   </div>
-    // );
+  return (
+    <div className="Result">
+      <BrowserRouter>
+        <Resultpage />
+        <Routes>
+          <Route path={"/result"} element={<Resultpage />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+  // return (
+  //   <div className="Quiz">
+  //     <BrowserRouter>
+  //       <Quizpage />
+  //       <Routes>
+  //         <Route path={"/quiz"} element={<Quizpage/>}></Route>
+  //       </Routes>
+  //     </BrowserRouter>
+  //   </div>
+  // );
+  // return (
+  //   <div className="Home">
+  //     <BrowserRouter>
+  //       <Homepage />
+  //       <Routes>
+  //         <Route path={"/home"} element={<Homepage/>}></Route>
+  //       </Routes>
+  //     </BrowserRouter>
+  //   </div>
+  // );
+  // return (
+  //   <div className="type">
+  //     <BrowserRouter>
+  //       <Typepage />
+  //       <Routes>
+  //         <Route path={"/type"} element={<Typepage/>}></Route>
+  //       </Routes>
+  //     </BrowserRouter>
+  //   </div>
+  // );
 }
