47456be2f2a951863c6fd7d48c760b1e683c10b4
이홍주 <hongju4385@gmail.com>
2023-03-06 12:42:36 +0900

    오타 수정

--- src/Answer/answer12.js
+++ src/Answer/answer12.js
@@ -20,14 +20,20 @@ export default function answer12() {
   return (
     <div className="App">
       <ProgressBar num="12" />
-      <div className="AnsTitle" style={{width: "90%"}}>
+      <div className="AnsTitle" style={{ width: "90%" }}>
         아닙니다. 0원 고지서 제출이 필요합니다!
-        <img className="titleImg" src="https://www.pknu.ac.kr/upload/raonkeditordata/2022/08/18/20220818_102123489_18337.png"></img>
+        <img
+          className="titleImg"
+          src="https://www.pknu.ac.kr/upload/raonkeditordata/2022/08/18/20220818_102123489_18337.png"
+        ></img>
       </div>
 
-      <div className="Ans normal" style={{top: "5vh", fontSize: "18px", lineHeight: "30px"}}>
-        등록금 고지서에 0원으로 조회되어도 은행 어플을 사용하거나
-        은행에 직접 방문하여 0원 고지서를 제출해야 합니다. 주의하세요! <br/>
+      <div
+        className="Ans normal"
+        style={{ top: "5vh", fontSize: "18px", lineHeight: "30px" }}
+      >
+        등록금 고지서에 0원으로 조회되어도 은행 어플을 사용하거나 은행에 직접
+        방문하여 0원 고지서를 제출해야 합니다. 주의하세요! <br />
         아니면 자신의 가상 계좌로 총학생회비 8000원만 납부해도 괜찮습니다!
       </div>
       <NextBtn />
