9521f32393f2117e4025ff5c0b6dd550531ba2c5
sujeong <qkrtnwjd4212@naver.com>
2023-03-06 12:41:59 +0900

    오타수정, 1번문제 수정

--- src/Answer/answer01.js
+++ src/Answer/answer01.js
@@ -1,10 +1,11 @@
 import "./Answer.css";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
 
 export function ProgressBar(props) {
   var num = 1;
   var guage = props.num * 10;
-
+  const count = useLocation();
+  console.log(count);
   return (
     <div className="progressBar">
       <div className="progressNum" value={props.num}>{`${props.num}/20`}</div>
@@ -32,37 +33,20 @@ export default function answer01() {
     <div className="App" style={{height: "120vh"}}>
       <ProgressBar num="1" />
       <div className="AnsTitle" style={{width: "90%"}}>
-        예비수강신청 후 신청이 가장 빠릅니다!
+        신청을 취소해도 여석이 바로 생기지 않으니 신중하게 결정하세요!
         <img className="titleImg" src="https://www.pknu.ac.kr/upload/raonkeditordata/2022/08/18/20220818_102123489_18337.png"></img>
       </div>
 
-      <div className="bold Ans" style={{ top: "5vh" }}>
-        1. 예비수강신청
-      </div>
-      <div className="normal Ans" style={{ top: "6vh" }}>
-        예비수강신청 기간에 신청해둔 과목들은, 본수강신청 때 수강 메뉴에
-        들어가면 바로 뜨기 때문에 가장 빠르게 신청할 수 있어요 !
-      </div>
-
-      <div className="bold Ans" style={{ top: "10vh" }}>
-        2. 학수번호 입력
-      </div>
-      <div className="normal Ans" style={{ top: "11vh" }}>
-        빠른수강신청 탭에서 학수번호 및 분반 9자리를 입력하는 방식으로도 신청할
-        수 있습니다. 예비수강신청 때 신청하지 못했거나, 플랜2 과목들의 경우
-        학수번호를 단축어로 추가해두면 편하겠죠 !
-      </div>
-
-      <div className="bold Ans" style={{ top: "15vh" }}>
-        3. PC 포털에서 신청
-      </div>
-      <div className="normal Ans" style={{ top: "16vh" }}>
-        부경포털의 학사행정정보시스템에서도 수강신청이 가능하지만, 모바일
-        환경보다 복잡해서 추천드리지 않습니다! 모바일 수강신청 어플이 훨씬
-        편해요 !
+      <div className="normal Ans" style={{ top: "5vh", lineHeight: "30px" }}>
+        누군가 수강신청한 과목을 취소할 경우, 해당 여석은 시스템에 즉시 반영되지 않습니다. <br/> 
+        취소했을 때 현원은 학사행정정보시스템의 수업시간표 및 강의계획서 조회 메뉴에서
+        실시간으로 확인이 가능하지만, 신청할 수 있는 여석은 랜덤한 시간 뒤에 생기는데요. <br/>
+        따라서 이루미에서 누군가 빠진 것을 확인하면 수강신청 앱에서 계속 새로고침하며
+        여석이 생기길 기다려야 합니다!
       </div>
 
       <NextBtn top="110vh" />
+
     </div>
   );
 }
